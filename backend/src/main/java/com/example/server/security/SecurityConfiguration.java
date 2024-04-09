74a3223409ad908e9a904718ce79161ce8e099e9
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-04-08 22:21:29 -0700

    Added folder to security configurations and other authorization related files

---
+++ backend/src/main/java/com/example/server/security/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.example.server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.csrfTokenRepository(
+                CookieCsrfTokenRepository.withHttpOnlyFalse() // CSRF token sent to every REST request that modifies API state
+            ))
+            .authorizeHttpRequests(authorize -> authorize
+                // Public endpoints
+                .requestMatchers(HttpMethod.POST, "users/register").permitAll()
+                .requestMatchers(HttpMethod.POST, "users/login").permitAll()
+                // Private endpoints
+                .anyRequest().authenticated()
+            )
+            .authenticationManager(authenticationManager(http));
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+
+}
