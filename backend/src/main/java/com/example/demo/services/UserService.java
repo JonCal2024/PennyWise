8a2e5c4dcdb6e892fcd811468730f1469a33f125
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-04-12 22:24:54 -0700

    Moved business logic originally in UserController to UserService file

--- backend/src/main/java/com/example/demo/services/UserService.java
+++ backend/src/main/java/com/example/demo/services/UserService.java
@@ -3,11 +3,13 @@ package com.example.demo.services;
 import java.util.Optional;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.User;
 import com.example.demo.interfaces.UserRepository;
 
 // Provides business logic, Controller should use instance of UserService
+@Service
 public class UserService {
     
     private UserRepository userRepository;
@@ -16,6 +18,19 @@ public class UserService {
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public User findByEmail(String email) throws Exception {
+        Optional<User> existingUser = userRepository.findByEmail(email);
+        if (existingUser.isPresent()) {
+            throw new Exception("User already exists");
+        }
+
+        return existingUser.get();
+    }
+    
+    public User findByID(String id) {
+        return userRepository.findByID(id).orElse(null);
     }
 
     public void signup(User user) throws Exception { // Logic for registering a user (Insert to DB)
@@ -27,12 +42,25 @@ public class UserService {
         }
 
         String hashedPassword = passwordEncoder.encode(user.getPassword());
-        // User registeredUser = new User(user.getUsername(), 
-        //                                hashedPassword, 
-        //                                email);
-        user.setPassword(hashedPassword);
-        userRepository.save(user);
+        User registeredUser = new User(user.getUsername(), 
+                                       hashedPassword, 
+                                       email);
+        userRepository.save(registeredUser);
 
+    }
+
+    public User updateUser(String id, String password) {
+        User user = userRepository.findByID(id).orElse(null);
+        if(password != null && user != null)
+        {
+            user.setPassword(password);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
     }
 
 }
