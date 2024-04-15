c74b4258a8cf54a8e8b5446136efee0ab9f2a3fe
Jonny Olswang <jtolswang@gmail.com>
2024-04-15 16:59:20 -0700

    Reverting ID type in UserRepository to ObjectId

--- backend/src/main/java/com/example/demo/interfaces/UserRepository.java
+++ backend/src/main/java/com/example/demo/interfaces/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.entities.User;
 
-public interface UserRepository extends MongoRepository<User, String>{
-    Optional<User> findByID(String id);
+public interface UserRepository extends MongoRepository<User, ObjectId>{
+    Optional<User> findByID(ObjectId id);
     Optional<User> findByEmail(String email); 
 }
