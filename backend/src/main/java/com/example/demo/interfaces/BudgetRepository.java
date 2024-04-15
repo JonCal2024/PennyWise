dfb4000c772089385513a5462528cbe195f9c86f
Jonny Olswang <jtolswang@gmail.com>
2024-04-15 16:52:08 -0700

    Added findByID() and findByUserID() in BudgetRepository, changed entity ID type from String to ObjectId in UserService and USerRepository

--- backend/src/main/java/com/example/demo/interfaces/BudgetRepository.java
+++ backend/src/main/java/com/example/demo/interfaces/BudgetRepository.java
@@ -1,10 +1,13 @@
 package com.example.demo.interfaces;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.entities.Budget;
 
 public interface BudgetRepository extends MongoRepository<Budget, ObjectId>{
-
-}
+    Optional<Budget> findByID(ObjectId id);
+    Optional<Budget> findByUserID(ObjectId user_id);
+} 
