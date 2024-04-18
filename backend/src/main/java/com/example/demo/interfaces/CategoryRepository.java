7f429007765faa9d01aaca3c28615412120e7e7d
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-04-17 20:13:53 -0700

    Created CategoryRepository

---
+++ backend/src/main/java/com/example/demo/interfaces/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.interfaces;
+
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.demo.entities.Category;
+
+public interface CategoryRepository extends MongoRepository<Category, ObjectId>{
+    Optional<Category> findByID(ObjectId id);
+}
