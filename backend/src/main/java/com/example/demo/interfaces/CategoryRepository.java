b1293ac88034f378d8c51754ece77308f9eeaaa6
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-04-25 14:07:03 -0700

    Removed deletByName from CategoryRepository

--- backend/src/main/java/com/example/demo/interfaces/CategoryRepository.java
+++ backend/src/main/java/com/example/demo/interfaces/CategoryRepository.java
@@ -5,12 +5,13 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.example.demo.entities.Category;
 
 public interface CategoryRepository extends MongoRepository<Category, ObjectId>{
     Optional<Category> findById(ObjectId id);
-    Optional<Category> findByName(String name);
-    List<Category> findByBudgetID(ObjectId budgetID);
-    void deleteByName(String name);
+    @Query("{ 'budget_id' : ?0 }")
+    List<Category> findByBudgetId(ObjectId budgetID);
+    void deleteById(ObjectId id);
 }
