bde40664ac7e3750b97a35618509f6b8e26086f0
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-04-25 13:02:42 -0700

    Added RequestBody for /updateBudget

--- backend/src/main/java/com/example/demo/controllers/BudgetController.java
+++ backend/src/main/java/com/example/demo/controllers/BudgetController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Budget;
@@ -47,7 +48,7 @@ public class BudgetController {
     }
 
     @PatchMapping("/updateBudget")
-    public Budget updateBudget(Budget budget)
+    public Budget updateBudget(@RequestBody Budget budget)
     {
         return budgetService.updateBudget(budget.getID(), budget);
     } 
