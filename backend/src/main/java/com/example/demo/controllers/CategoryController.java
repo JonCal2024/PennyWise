3d4217cae5f2832c03a0bb7c63e9f9b8a632ecb6
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-04-25 14:08:57 -0700

    Re-added updateCategory & getCategoryById to CategoryService, Modified getAllCategories

--- backend/src/main/java/com/example/demo/controllers/CategoryController.java
+++ backend/src/main/java/com/example/demo/controllers/CategoryController.java
@@ -6,6 +6,8 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,15 +32,26 @@ public class CategoryController {
 
     }
 
-    @DeleteMapping("/removeCategory")
-    public void removeCategory(@RequestBody Category category) {
+    @DeleteMapping("/{id}")
+    public void removeCategory(@PathVariable ObjectId id) {
+        categoryService.removeCategory(id);
+    }
 
-        categoryService.removeCategory(category);
-
+    @GetMapping("/{id}")
+    public Category getCategory(@PathVariable ObjectId id) {
+        return categoryService.getCategoryById(id);
     }
 
     @GetMapping("/getAllCategories")
-    public List<Category> getAllCategories(ObjectId budgetID) {
-        return categoryService.findAllCategoriesByBudgetID(budgetID);
+    public List<Category> getAllCategories(String budgetID) {
+
+        ObjectId userBudget = new ObjectId(budgetID);
+        return categoryService.findAllCategoriesByBudgetID(userBudget);
     }
+
+    @PatchMapping("/updateCategory")
+    public Category updateCategory(@RequestBody Category category)
+    {
+        return categoryService.updateCategory(category.getID(), category);
+    } 
 }
