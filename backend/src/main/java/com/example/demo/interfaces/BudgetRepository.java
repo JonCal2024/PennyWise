2ed544b50174e9f7c5b355d6809704e49a91e872
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-04-24 22:25:35 -0700

    Added custom query for getting all budgets for user

--- backend/src/main/java/com/example/demo/interfaces/BudgetRepository.java
+++ backend/src/main/java/com/example/demo/interfaces/BudgetRepository.java
@@ -5,11 +5,13 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.example.demo.entities.Budget;
 
 public interface BudgetRepository extends MongoRepository<Budget, ObjectId>{
     Optional<Budget> findById(ObjectId id);
-    List<Budget> findByUserID(ObjectId user_id);
+    @Query("{ 'user_id' : ?0 }")
+    List<Budget> findByUserId(ObjectId user_id);
     void deleteById(ObjectId id);
 }  
