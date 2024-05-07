4c35a4c391685e53a4bacd51043bfe54f3d66206
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-05-06 22:29:22 -0700

    Cleaned up JWT file

--- backend/src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -33,13 +33,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
           token = authHeader.substring(7);
           username = JwtHelper.extractEmail(token);
         }
-  //      If the accessToken is null. It will pass the request to next filter in the chain.
-  //      Any login and signup requests will not have jwt token in their header, therefore they will be passed to next filter chain.
         if (token == null) {
           filterChain.doFilter(request, response);
           return;
         }
-  //       If any accessToken is present, then it will validate the token and then authenticate the request in security context
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
           UserDetails userDetails = userDetailsService.loadUserByUsername(username);
           if (JwtHelper.validateToken(token, userDetails)) {
@@ -48,7 +45,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
           }
         }
-  
         filterChain.doFilter(request, response);
       } catch (Exception e) {}
     }
